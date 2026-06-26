daa8c67cbfaf7f1a7b8ba3822036aebdd02429f1
sj3101 <srshtijain17@gmail.com>
2026-06-26 17:55:11 +0530

    remove duplicate root files and cleanup project structure

--- src/db/enums.js
+++
@@ -1,9 +0,0 @@
-const { pgEnum } = require('drizzle-orm/pg-core');
-
-const roleEnum = pgEnum('role_enum', ['EMP', 'RM', 'APE', 'CFO']);
-const approvalStatusEnum = pgEnum('approval_status_enum', ['PENDING', 'APPROVED', 'REJECTED']);
-
-module.exports = {
-  roleEnum,
-  approvalStatusEnum,
-};
